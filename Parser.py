8e85ab199f79c2a4bc5abe895c34f917331b5de4
Ido Greenberg <idogreenberg90@gmail.com>
2019-06-24 01:19:51 +0300

    Scalable parser + PCA

--- Parser.py
+++ Parser.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from pprint import pprint
 from warnings import warn
 from datetime import datetime
+from tqdm import tqdm
 import itertools
 from collections import Counter, OrderedDict
 from bidi import algorithm as bidi
@@ -20,20 +21,125 @@ from hebrew_stopwords import hebrew_stopwords
 
 EXTRA_STOPWORDS = (
     'לך',
+    'רוצה',
+    'צריך',
+    'הכנסת',
+    'היום',
+    'משרד',
+    'שיש',
+    'שזה',
+    'דבר',
+    'אומר',
+    'החוק',
+    'הזאת',
+    'לגבי',
+    'ואני',
+    'לעשות',
+    'שאנחנו',
+    'האלה',
+    'חוק',
+    'הוועדה',
+    'אפשר',
+    'שאני',
+    'כמה',
+    'עכשיו',
+    'חושב',
+    'הרבה',
+    'אלא',
+    'לפי',
+    'וגם',
+    'סעיף',
+    'האם',
+    'שהם',
+    'נכון',
+    'ראש',
+    'המדינה',
+    'רוצים',
+    'הממשלה',
+    'יודע',
+    'ועדת',
+    'הדברים',
+    'הנושא',
+    'בנושא',
+    'היור',
+    'חבר',
+    'צריכים',
+    'שהיא',
+    'דברים',
+    'אנשים',
+    'וזה',
+    'אומרת',
+    'תודה',
+    'קודם',
+    'באמת',
+    'להגיד',
+    'לקבל',
+    'שר',
+    'משהו',
+    'לזה',
+    'לומר',
+    'אדוני',
+    'טוב',
+    'לכל',
+    'בחוק',
+    'שום',
+    'מבקש',
+    'הדיון',
+    'שצריך',
+    'מדובר',
+    'נושא',
+    'ואנחנו',
+    'מדברים',
+    'חלק',
+    'באופן',
+    'הצעת',
+    'בסדר',
+    'חברי',
+    'הייתי',
+    'חשוב',
+    'יודעים',
+    'בעד',
+    'שאתה',
+    'והוא',
+    'שאין',
+    'עושים',
+    'בכלל',
+    'בבקשה',
+    'הדבר',
+    'העניין',
+    'בצורה',
+    'בזה',
+    'בעצם',
+    'ויש',
+    'ואז',
+    'בסעיף',
+    'מדבר',
+    'במקום',
+    'ולכן',
+    'צריכה',
+    'כרגע',
+    'כאלה',
+    'בעניין',
+    'רבה',
+    'חושבת',
+    'בדיוק',
+    'רואה',
+    'יהיו',
+    'ידי',
+    'עדיין'
 )
 
 
 WORDS_SEPS = ' | - |–|\t|\n\r|\n'
-CHARS_TO_FILTER = '\.|,|"|\(|\)|;|:|\?|\t'
+CHARS_TO_FILTER = '\.|,|"|\(|\)|;|:|\?|\t' # TODO should "'" be included?
 
 
 def ordered_counter(tokens):
-	return OrderedDict(sorted(Counter([tok for grp in tokens for tok in grp]).items(),
+    return OrderedDict(sorted(Counter([tok for grp in tokens for tok in grp]).items(),
                               key=lambda kv: kv[1], reverse=1))
 
 def tokenize(texts, seps=WORDS_SEPS, chars_to_filter=CHARS_TO_FILTER,
              stopwords=hebrew_stopwords, filter_fun=None, apply_fun=None):
-    
     words = [re.split(seps,txt) for txt in texts]
     words = [re.sub(chars_to_filter, '', w.strip())
              for text_words in words for w in text_words]
@@ -52,9 +158,9 @@ class Parser:
                  words_seps=WORDS_SEPS, chars_to_filter=CHARS_TO_FILTER,
                  filter_fun=lambda s: len(s)>1 or '0'<=s<='9', apply_fun=None,
                  stopwords=hebrew_stopwords+EXTRA_STOPWORDS,
-                 max_voc_size=1000, min_voc_occurences=6,
+                 max_voc_size=1000, min_voc_occurences=10,
                  laplace_smooth=0,
-                 do_init=True):
+                 do_init=1):
         # data
         self.meta = meta
         self.df = df
@@ -67,6 +173,7 @@ class Parser:
         self.apply_fun = apply_fun # TODO use this for lemmatization
         self.stopwords = stopwords
         self.tokens = None
+        self.profile = None
         # vocabulary
         self.max_voc_size = max_voc_size
         self.min_voc_occurences = min_voc_occurences
@@ -74,32 +181,63 @@ class Parser:
         # one hot encoding
         self.laplace_smooth = laplace_smooth
         self.one_hot = None
-        
+
         if do_init:
-            self.tokenize()
+            if do_init > 1:
+                self.tokenize() # hold list of tokens per text - less efficient
+            else:
+                self.corpus_profile()
             self.set_vocabulary()
             self.set_one_hot_embedding()
-    
+
     # Parsing
+    def corpus_profile(self):
+        self.profile = {}
+        for text in tqdm(self.df.body):
+            for w in tokenize([text], self.words_seps, self.chars_to_filter,
+                              self.stopwords, self.filter_fun, self.apply_fun):
+                self.profile[w] = self.profile[w]+1 if w in self.profile else 1
+        self.profile = OrderedDict(sorted(self.profile.items(), key=lambda kv: kv[1], reverse=True))
+
     def tokenize(self, by='ID'):
         self.tokens = self.df.groupby(by).apply(lambda d: tokenize(d.body.values, self.words_seps, self.chars_to_filter,
                                                                    self.stopwords, self.filter_fun, self.apply_fun))
     
     def set_vocabulary(self):
-        all_tokens = [tok for group in self.tokens for tok in group]
-        counter = OrderedDict(sorted(Counter(all_tokens).items(), key=lambda kv: kv[1], reverse=1))
+        if self.profile:
+            counter = self.profile
+        else:
+            all_tokens = [tok for group in self.tokens for tok in group]
+            counter = OrderedDict(sorted(Counter(all_tokens).items(), key=lambda kv: kv[1], reverse=True))
         voc = list(counter.keys())
         if self.max_voc_size:
+            print(f"Vocabulary: {self.max_voc_size:d}'th word => {list(counter.values())[self.max_voc_size]:d} occurences.")
             voc = voc[:self.max_voc_size]
         if self.min_voc_occurences:
             i = int(np.sum(np.array(list(counter.values()))>=self.min_voc_occurences))
+            print(f"Vocabulary: {self.min_voc_occurences:d} occurences => {i:d} words.")
             voc = voc[:i]
         self.vocabulary = voc
-            
+
     def set_one_hot_embedding(self):
+        if not self.tokens:
+            self.set_one_hot_on_the_fly()
+            return
         counters = {w: len(self.tokens)*[self.laplace_smooth] for w in self.vocabulary}
         for i,group in enumerate(self.tokens):
             for w in group:
+                if w in counters:
+                    counters[w][i] += 1
+        self.one_hot = pd.DataFrame({**counters})
+
+    def set_one_hot_on_the_fly(self):
+        filter_fun = self.filter_fun if self.filter_fun else lambda w: w in self.vocabulary
+        counters = {w: len(np.unique(self.df.ID)) * [self.laplace_smooth]
+                    for w in self.vocabulary}
+        for i,ID in tqdm(enumerate(np.unique(self.df.ID))):
+            for w in tokenize(self.df[self.df.ID==ID].body.values,
+                              self.words_seps, self.chars_to_filter,
+                              self.stopwords, filter_fun, self.apply_fun):
                 if w in counters:
                     counters[w][i] += 1
         self.one_hot = pd.DataFrame({**counters})
@@ -112,7 +250,11 @@ class Parser:
         return '\n\n'.join(['________\n'+h+':\n'+b for h,b in zip(protocol.header,protocol.body)])
     
     def show_tokens(self, **kwargs):
-        show_freqs([tok for group in self.tokens for tok in group], **kwargs)
+        if self.profile:
+            show_freqs(self.profile, **kwargs)
+        else:
+            show_freqs([tok for group in self.tokens for tok in group], **kwargs)
+
         
     def one_hot_pca(self):
         # TODO plot PCA of one_hot with 2 components
@@ -128,7 +270,7 @@ def show_freqs(tokens, vocabulary=None, n=15, logscale=True, token_lab='Token', 
     if vocabulary:
         vocabulary = set(vocabulary)
         tokens = [token for token in  tokens if token in vocabulary]
-    count = Counter(tokens)
+    count = tokens if tokens is dict else Counter(tokens)
     count = OrderedDict(sorted(count.items(), key=lambda kv: kv[1]))
     vals = list(count.values())
     keys = list(count.keys())
@@ -140,7 +282,7 @@ def show_freqs(tokens, vocabulary=None, n=15, logscale=True, token_lab='Token', 
     ax.set_xlabel('Quantile [%]')
     ax.set_ylabel('Number of occurences')
     ax.set_xlim((0,100))
-    ax.set_title(f'Total: {len(tokens):d}, Unique: {len(keys):d}')
+    ax.set_title(f'Total: {np.sum(vals):.0f}, Unique: {len(keys):d}')
     if logscale:
         ax.set_yscale('log')
     ax.grid()
